937270e6de2b2c690a2f08783085d276559aba8a
cmatthews444 <155667279+cmatthews444@users.noreply.github.com>
2025-06-10 14:25:37 -0700

    added character name form for appview

--- app/views/app_view.py
+++ app/views/app_view.py
@@ -2,7 +2,7 @@
 
 """This module defines the UI components that go into our main app page."""
 
-from PyQt6.QtWidgets import QWidget, QLabel, QVBoxLayout
+from PyQt6.QtWidgets import QWidget, QLabel, QVBoxLayout, QLineEdit
 
 class AppView(QWidget):
     """
@@ -12,9 +12,17 @@ class AppView(QWidget):
     def __init__(self, name: str):
         super().__init__()
 
-        message = QLabel(f"Hello, {name}!" if name else "Hello!")
+        message = QLabel(f"Hello, {name}! Now choose your character info!" if name else "Hello!")
         message.setStyleSheet("font-size: 20px; color: green")
 
         layout = QVBoxLayout()
         layout.addWidget(message)
+
+
+        self.Character_name_input = QLineEdit()
+        self.Character_name_input.setPlaceholderText("Enter your character name")
+        layout.addWidget(QLabel("Character Name:"))
+        layout.addWidget(self.Character_name_input)
+
+
         self.setLayout(layout)
